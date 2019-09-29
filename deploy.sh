357b3c692a51b099c443c9dc45d7e4738e461682
root <millercollin07@gmail.com>
2019-09-28 23:47:47 -0500

    dev

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker stop $(docker ps -a -q)
 #docker rm $(docker ps -a -q)
 #docker rmi $(docker images -q)
-docker build -t nm1 .
+docker pull millercollin07/nm1 
 docker run -p 5000:5000 -t nm1
