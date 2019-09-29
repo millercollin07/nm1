5d64f55bf13e7b26f19a50f48300b51f0bdeb8fb
root <millercollin07@gmail.com>
2019-09-29 03:12:32 -0500

    dev

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps Challenge
+          NM DevOps Challenge
         </p>
         <a
           className="App-link"
