852b4f54f1b9df5032055e39915e3b7e365c7589
root <root@nmguest.rackhamjack.com>
2019-09-30 13:32:04 -0500

    edit message

--- src/App.js
+++ src/App.js
@@ -10,10 +10,9 @@ function App() {
         <h1>
           DevOps Challenge
         </h1>
-        <p>
-	  Ci pipline uses git hub which triggers travis ci which triggers</p>
-	  <p>uploads docker image to Docker Hub upon successful image build and testing
-	  </p>
+        <p>Ci pipline uses git hub which triggers travis ci which triggers</p>
+	  <p>the upload of a docker image to Docker Hub upon successful</p>
+	  <p>image build and testing</p>
       </header>
     </div>
   );
