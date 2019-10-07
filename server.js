a0872819adbd2f2aaf60c03d7c7582aa3fc65bbb
Collin Miller <root@nmguest.rackhamjack.com>
2019-10-07 06:44:31 -0500

    health

--- server.js
+++ server.js
@@ -8,4 +8,10 @@ app.get('/', function(req, res) {
 	  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+app.get('/health', function(req, res) {
+		  res.sendStatus(200);
+});
+
+
+
 app.listen(5000);
